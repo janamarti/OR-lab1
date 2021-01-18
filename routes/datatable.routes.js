779c4add6ac08edaf1842eb3df10652974637d01
jana.martinovic <jana.martinovic@fer.hr>
2021-01-18 16:22:22 +0100

    lab 4

--- routes/datatable.routes.js
+++ routes/datatable.routes.js
@@ -7,21 +7,28 @@ var cors = require('cors');
 router.use(cors());
 router.use(hateoasLinker);   
 
+const request = require('request')
+const fetch = require('node-fetch');
+var fs = require('fs');
 
 
 
 router.get('/drzave', async function (req, res, next) {
- 
-    const sqlDrzave = `SELECT * FROM drzava;`;
 
+    const sqlDrzave = `SELECT * FROM drzava;`;
     hateoasLinker.apply;
     try {
         const resultDrzave = (await db.query(sqlDrzave, [])).rows;
-
+        for (let drzava of resultDrzave){
+          drzava.slika = "localhost:3000/drzave/" + drzava.iso_3116_alpha_3 + "/slike";
+        }
         res.status(200).json({
           status:"OK",
           message:"Drzave su uspjesno dohvacene",
-          response:resultDrzave,
+          response:{"@context": {
+            "glavni_grad": "https://schema.org/City",
+            "valuta": "https://schema.org/currency"
+          }, resultDrzave},
           links:[  
       { rel: "self", method: "GET", href: '/drzave' },
       { rel: "read", method: "GET", title: 'GET svi jezici', href: '/jezici' }
@@ -61,12 +68,17 @@ router.get('/drzave/:id', async function (req, res, next) {
   hateoasLinker.apply;
   try {
       const resultDrzavaId = (await db.query(sqlDrzavaId, [])).rows;
+      
+      resultDrzavaId[0].slika  = "localhost:3000/drzave/" + req.params.id + "/slike";
 
       if(resultDrzavaId.length){
         res.status(200).json({
           status:"OK",
           message:"Drzava je uspjesno dohvacena",
-          response:resultDrzavaId,
+          response:{"@context": {
+            "glavni_grad": "https://schema.org/City",
+            "valuta": "https://schema.org/currency"
+          }, resultDrzavaId},
           links:[  
       { rel: "read", method: "GET", title: 'GET sluzbeni jezici ove drzave', href: '/drzave/'+req.params.id+'/sluzbenijezici' },
       { rel: "read", method: "GET", title: 'GET sve drzave', href: '/drzave' }  
@@ -171,7 +183,10 @@ router.delete('/drzave/:id', async function (req, res, next) {
         res.status(200).json({
           status:"OK",
           message:"Drzava je uspjesno obrisana",
-          response:resultDrzavaId,
+          response:{"@context": {
+            "glavni_grad": "https://schema.org/City",
+            "valuta": "https://schema.org/currency"
+          }, resultProvjera},
           links:[  
       { rel: "read", method: "GET", title: 'GET sve drzave', href: '/drzave' }  
   ]})}
@@ -198,13 +213,15 @@ router.post('/drzave/:id',bodyParser.json(), async function (req, res, next) {
   console.log(body);
 
   const values = "('" + req.params.id + "', '" + body.glavni_grad + "', '" + body.naziv + "', '" + body.valuta + "', '" + body.pozivni_broj + "', '" + 
-                  body.top_level_domena + "', '" + body.naziv_himne + "', '" + body.povrsina + "', '" + body.strana_voznje + "', '" + body.wikipedia + "')"
+                  body.top_level_domena + "', '" + body.naziv_himne + "', '" + body.povrsina + "', '" + body.strana_voznje + "', '" + body.wikipedia_handle + "')"
  
   const sqlProvjera = `SELECT *  FROM drzava 
-                      WHERE iso_3116_alpha_3 = '` + req.params.id + `'`;
+                  WHERE iso_3116_alpha_3 = '` + req.params.id + `'`;
 
                       //INSERT INTO public.drzava VALUES ('PLW', 'Ngerulmud', 'Palau', 'USD', '+680', '.pw', 'Belau rekid', 459, 'desna', 'Palau');
   const sqlDrzavaId = `INSERT INTO public.drzava VALUES` + values + `;`;
+
+
 
   console.log(sqlDrzavaId);
   console.log(sqlProvjera);
@@ -212,18 +229,27 @@ router.post('/drzave/:id',bodyParser.json(), async function (req, res, next) {
   
   hateoasLinker.apply;
   try {
+      
+
       const resultProvjera = (await db.query(sqlProvjera, [])).rows;
       console.log("resultProvjera: " + resultProvjera);
       if(!resultProvjera.length){
         const resultDrzavaId = (await db.query(sqlDrzavaId, [])).rows;
         console.log("resultDrzavaId: " + resultDrzavaId);
+        const resultProvjera = (await db.query(sqlProvjera, [])).rows;
+
+        const sqlProvjera2 = `SELECT *  FROM drzava 
+      WHERE iso_3116_alpha_3 = '` + req.params.id + `'`;
 
         console.log(resultProvjera);
 
         res.status(200).json({
           status:"OK",
           message:"Drzava je uspjesno stvorena",
-          response:resultDrzavaId,
+          response:{"@context": {
+            "glavni_grad": "https://schema.org/City",
+            "valuta": "https://schema.org/currency"
+          }, resultProvjera},
           links:[  
       { rel: "read", method: "GET", title: 'GET sve drzave', href: '/drzave' }  
   ]})}
@@ -250,8 +276,9 @@ router.put('/drzave/:id',bodyParser.json(), async function (req, res, next) {
 
   sqlDrzaveUpdate = "UPDATE drzava SET       glavni_grad = '" + body.glavni_grad + "'," + 
                     "naziv = '" + body.naziv + "', valuta = '" + body.valuta + "', pozivni_broj = '" + body.pozivni_broj + "', top_level_domena = '" + body.top_level_domena
-                    + "', naziv_himne = '" + body.naziv_himne + "', povrsina = '" + body.povrsina + "', strana_voznje = '" + body.strana_voznje + "', wikipedia_handle = '" + body.wikipedia
-                    + "' WHERE iso_3116_alpha_3 = '" + req.params.id + "';";
+                    + "', naziv_himne = '" + body.naziv_himne + "', povrsina = '" + body.povrsina + "', strana_voznje = '" + body.strana_voznje + "', wikipedia_handle = '" + body.wikipedia_handle
+                    + "' WHERE iso_3116_alpha_3 = '" + req.params.id + "';"
+
  
   const sqlProvjera = `SELECT *  FROM drzava 
                       WHERE iso_3116_alpha_3 = '` + req.params.id + `'`;
@@ -263,15 +290,21 @@ router.put('/drzave/:id',bodyParser.json(), async function (req, res, next) {
   hateoasLinker.apply;
   try {
     const resultProvjera = (await db.query(sqlProvjera, [])).rows;
+    console.log("PROVJERA:" + resultProvjera);
 
     if(resultProvjera.length){
       
       const resultDrzaveUpdate = (await db.query(sqlDrzaveUpdate, [])).rows;
+      const resultUpdateanaDrzava = (await db.query(sqlProvjera, [])).rows;
+      console.log("RESULT:" + resultUpdateanaDrzava);
 
       res.status(200).json({
         status:"OK",
         message:"Drzava je uspjesno updateana",
-        response:resultDrzaveUpdate,
+        response:{"@context": {
+          "glavni_grad": "https://schema.org/City",
+          "valuta": "https://schema.org/currency"
+        }, resultUpdateanaDrzava},
         links:[  
           { rel: "read", method: "GET", title: 'GET ovu drzavu', href: '/drzave/' + req.params.id },
     { rel: "read", method: "GET", title: 'GET sluzbeni jezici ove drzave', href: '/drzave/'+req.params.id+'/sluzbenijezici' },
@@ -290,6 +323,76 @@ router.put('/drzave/:id',bodyParser.json(), async function (req, res, next) {
 });
 
 
+router.get('/drzave/:id/slike', async function (req, res, next) {
+  res.header("Content-Type","image/png")
+
+  const sqlSlike = `SELECT * FROM slika
+                      WHERE iso_3116_alpha_3 ='` + req.params.id + `'`;
+  const sqlDrzave = `SELECT * FROM drzava
+                      WHERE iso_3116_alpha_3 = '` + req.params.id + "'"; 
+  hateoasLinker.apply;
+  try {
+      const resultSlike = (await db.query(sqlSlike, [])).rows;
+      const resultDrzave = (await db.query(sqlDrzave, [])).rows;
+
+      let slika = resultSlike[0];
+      var novo_vrijeme = new Date();
+      var staro_vrijeme = new Date(slika.vrijeme);
+      let limit = 10800000 //3h u milisekundama
+      limit = 10;
+
+
+      console.log(slika.vrijeme);
+      console.log(novo_vrijeme);
+      
+      let razlika = novo_vrijeme - staro_vrijeme;
+      console.log("razlika: " + razlika + "limit: " + limit);
+
+      console.log(razlika > limit);
+
+
+
+      if(razlika > limit){
+        for (drzava of resultDrzave){
+
+          let uri = "https://en.wikipedia.org/api/rest_v1/page/summary/" + drzava.wikipedia_handle
+          let pic = await (await fetch(uri)).json();
+          pic = JSON.parse(JSON.stringify(pic));
+
+          let source = pic.originalimage.source;
+          let new_path = 'C:/Users/marti/OneDrive/Desktop/drzave/drzave/local_images/' + drzava.iso_3116_alpha_3 + '.png';
+          
+          download(source, new_path, () => {
+            console.log(new_path);
+            res.sendFile(new_path);
+          })
+
+          let sqlUpdateSlike = `UPDATE slika SET put='` + new_path + `', vrijeme=CURRENT_TIMESTAMP WHERE iso_3116_alpha_3='` + drzava.iso_3116_alpha_3 +`';`;
+            console.log(sqlUpdateSlike);
+            const updateSlike = (await db.query(sqlUpdateSlike, [])).rows;
+        }
+      }
+      else{
+        res.sendFile(slika.put);
+      } 
+
+      
+  
+} catch (err) {
+  console.log(err);
+}
+});
+
+
+const download = (url, path, callback) => {
+  request.head(url, (err, res, body) => {
+    request(url)
+      .pipe(fs.createWriteStream(path))
+      .on('close', callback)
+  })
+}
+
+
 router.use(function(req,res){
   res.status(501).json({
         status:"Not Implemented",
@@ -297,6 +400,7 @@ router.use(function(req,res){
         response:null
     });
     });
+
 
 
   
